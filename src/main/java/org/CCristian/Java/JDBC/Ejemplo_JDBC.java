fe0c8cdd05ddd1f7421745d7a8def25771e97364
CCrisstian <crisstiann.c@gmail.com>
2024-04-03 17:00:27 -0300

    Integración de la interfaz 'Service' completa

--- src/main/java/org/CCristian/Java/JDBC/Ejemplo_JDBC.java
+++ src/main/java/org/CCristian/Java/JDBC/Ejemplo_JDBC.java
@@ -2,53 +2,33 @@ package org.CCristian.Java.JDBC;
 
 import org.CCristian.Java.JDBC.Models.Categoria;
 import org.CCristian.Java.JDBC.Models.Producto;
-import org.CCristian.Java.JDBC.Repositorio.Categoria_Repositorio_Implement;
-import org.CCristian.Java.JDBC.Repositorio.Producto_Repositorio_Implement;
-import org.CCristian.Java.JDBC.Repositorio.Repositorio;
-import org.CCristian.Java.JDBC.Util.Conexion_BaseDeDatos;
+import org.CCristian.Java.JDBC.Service.CatalogoServicio;
+import org.CCristian.Java.JDBC.Service.Service;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Date;
 
 public class Ejemplo_JDBC {
     public static void main(String[] args) throws SQLException {
-        try (Connection conn = Conexion_BaseDeDatos.getConnection()) {
-            if (conn.getAutoCommit()){
-                conn.setAutoCommit(false);
-            }
-            try {
-                Repositorio<Categoria> repositorioCategoria = new Categoria_Repositorio_Implement(conn);
-                System.out.println("============= Insertar nueva categoria =============");
-                Categoria categoria = new Categoria();
-                categoria.setNombre("Electrohogar");
-                Categoria nuevaCategoria = repositorioCategoria.guardar(categoria);
-                System.out.println("Categoria guardada con éxito: " + nuevaCategoria.getId());
 
-                Repositorio<Producto> repositorio = new Producto_Repositorio_Implement(conn);
-                System.out.println("============= listar =============");
-                repositorio.listar().forEach(System.out::println);
+        Service servicio = new CatalogoServicio();
 
-                System.out.println("============= obtener por id =============");
-                System.out.println(repositorio.buscarPorId(1L));
+        System.out.println("Listar");
+        servicio.listar().forEach(System.out::println);
 
-                System.out.println("============= insertar nuevo producto =============");
-                Producto producto = new Producto();
-                producto.setNombre("Refrigerador Samsung");
-                producto.setPrecio(9900);
-                producto.setFecha_registro(new Date());
-                producto.setSku("abcdefg123");
+        System.out.println("\nInsertar nueva categoría");
+        Categoria categoria = new Categoria();
+        categoria.setNombre("Iluminación");
 
-                producto.setCategoria(nuevaCategoria);
-                repositorio.guardar(producto);
-                System.out.println("Producto guardado con éxito: " + producto.getId());
-                repositorio.listar().forEach(System.out::println);
+        System.out.println("\nInsertar nuevo producto");
+        Producto producto = new Producto();
+        producto.setNombre("Lámpara led escritorio");
+        producto.setPrecio(990);
+        producto.setFecha_registro(new Date());
+        producto.setSku("abcdefgh12");
 
-                conn.commit();
-            } catch (SQLException e){
-                conn.rollback();
-                e.printStackTrace();
-            }
-        }
+        servicio.guardarProductoConCategoria(producto, categoria);
+        System.out.println("Producto guardado con éxito: " + producto.getId());
+        servicio.listar().forEach(System.out::println);
     }
 }
--- src/main/java/org/CCristian/Java/JDBC/Service/CatalogoServicio.java
+++ src/main/java/org/CCristian/Java/JDBC/Service/CatalogoServicio.java
@@ -5,7 +5,9 @@ import org.CCristian.Java.JDBC.Models.Producto;
 import org.CCristian.Java.JDBC.Repositorio.Categoria_Repositorio_Implement;
 import org.CCristian.Java.JDBC.Repositorio.Producto_Repositorio_Implement;
 import org.CCristian.Java.JDBC.Repositorio.Repositorio;
+import org.CCristian.Java.JDBC.Util.Conexion_BaseDeDatos;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -21,46 +23,127 @@ public class CatalogoServicio implements Service{
 
     @Override
     public List<Producto> listar() throws SQLException {
-        return null;
+        try (Connection conn = Conexion_BaseDeDatos.getConnection()){
+            productoRepositorio.setConn(conn);
+            return productoRepositorio.listar();
+        }
     }
 
     @Override
     public Producto porId(Long id) throws SQLException {
-        return null;
+        try (Connection conn = Conexion_BaseDeDatos.getConnection()){
+            productoRepositorio.setConn(conn);
+            return productoRepositorio.buscarPorId(id);
+        }
     }
 
     @Override
     public Producto guardar(Producto producto) throws SQLException {
-        return null;
+        try (Connection conn = Conexion_BaseDeDatos.getConnection()){
+            productoRepositorio.setConn(conn);
+            if (conn.getAutoCommit()){
+                conn.setAutoCommit(false);
+            }
+            Producto nuevoProducto = null;
+            try {
+                nuevoProducto = productoRepositorio.guardar(producto);
+                conn.commit();
+            }catch (SQLException e){
+                conn.rollback();
+                e.printStackTrace();
+            }
+            return nuevoProducto;
+        }
     }
 
     @Override
     public void eliminar(Long id) throws SQLException {
+        try (Connection conn = Conexion_BaseDeDatos.getConnection()){
+            productoRepositorio.setConn(conn);
+            if (conn.getAutoCommit()){
+                conn.setAutoCommit(false);
+            }
+            try {
+                productoRepositorio.eliminar(id);
+                conn.commit();
+            }catch (SQLException e){
+                conn.rollback();
+                e.printStackTrace();
+            }
+        }
 
     }
 
     @Override
     public List<Categoria> listarCategoria() throws SQLException {
-        return null;
+        try (Connection conn = Conexion_BaseDeDatos.getConnection()){
+            categoriaRepositorio.setConn(conn);
+            return categoriaRepositorio.listar();
+        }
     }
 
     @Override
     public Categoria porIdCategoria(Long id) throws SQLException {
-        return null;
+        try (Connection conn = Conexion_BaseDeDatos.getConnection()){
+            categoriaRepositorio.setConn(conn);
+            return categoriaRepositorio.buscarPorId(id);
+        }
     }
 
     @Override
-    public Categoria guardarCategoria() throws SQLException {
-        return null;
+    public Categoria guardarCategoria(Categoria categoria) throws SQLException {
+        try (Connection conn = Conexion_BaseDeDatos.getConnection()){
+            categoriaRepositorio.setConn(conn);
+            if (conn.getAutoCommit()){
+                conn.setAutoCommit(false);
+            }
+            Categoria nuevaCategoria = null;
+            try {
+                nuevaCategoria = categoriaRepositorio.guardar(categoria);
+                conn.commit();
+            }catch (SQLException e){
+                conn.rollback();
+                e.printStackTrace();
+            }
+            return nuevaCategoria;
+        }
     }
 
     @Override
     public void eliminarCategoria(Long id) throws SQLException {
+        try (Connection conn = Conexion_BaseDeDatos.getConnection()){
+            categoriaRepositorio.setConn(conn);
+            if (conn.getAutoCommit()){
+                conn.setAutoCommit(false);
+            }
+            try {
+                categoriaRepositorio.eliminar(id);
+                conn.commit();
+            }catch (SQLException e){
+                conn.rollback();
+                e.printStackTrace();
+            }
+        }
 
     }
 
     @Override
     public void guardarProductoConCategoria(Producto producto, Categoria categoria) throws SQLException {
-
+        try (Connection conn = Conexion_BaseDeDatos.getConnection()){
+            productoRepositorio.setConn(conn);
+            categoriaRepositorio.setConn(conn);
+            if (conn.getAutoCommit()){
+                conn.setAutoCommit(false);
+            }
+            try {
+                Categoria nuevaCategoria = categoriaRepositorio.guardar(categoria);
+                producto.setCategoria(nuevaCategoria);
+                productoRepositorio.guardar(producto);
+                conn.commit();
+            }catch (SQLException e){
+                conn.rollback();
+                e.printStackTrace();
+            }
+        }
     }
 }
